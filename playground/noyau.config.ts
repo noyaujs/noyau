84fb6e2656d2af4e7f77234f09880404555444d4
Aaron Bassett <arbassett4@outlook.com>
2023-05-29 13:52:18 -0400

    better playground demo

--- playground/noyau.config.ts
+++ playground/noyau.config.ts
@@ -1,6 +1,7 @@
 import { defineNoyauConfig } from "@noyau/kit/config";
 
 export default defineNoyauConfig({
+  srcDir: "src",
   modules: ["@noyau/module-spa", "@noyau/module-react"],
   // @ts-expect-error noyau doesn't generate types for modules just yet
   spa: {
