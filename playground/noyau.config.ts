6f03350a3246952a91d6b373506478255ef95c43
Aaron Bassett <arbassett4@outlook.com>
2023-06-01 12:23:41 -0400

    spelling

--- playground/noyau.config.ts
+++ playground/noyau.config.ts
@@ -5,7 +5,7 @@ export default defineNoyauConfig({
   modules: [
     "@noyau/module-spa", // renderer
     "@noyau/module-react", // hmr
-    "@noyau/module-tanstack-router", // generate routes with @tanstack/router node: only components are implemented for now
+    "@noyau/module-tanstack-router", // generate routes with @tanstack/router. note: only components are implemented for now
     "@noyau/module-routes", // standardized filesystem routes
   ],
   spa: {
